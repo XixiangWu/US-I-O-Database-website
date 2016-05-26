ddd801fddfce02dd41f6b064ecbac36a8cf2201b
Xixiang Wu <xindefenbao1@126.com>
2016-05-26 20:29:09 +1000

    Update PTB

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import os
 import sqlite3 #Database management library we used
 import jinja2
-
+import pandas as pd
 
 #for row in c.execute('SELECT * FROM USIODB'):
 #    print row
@@ -23,63 +23,81 @@ def pivot_table():
     c = conn.cursor()
 
     table = html_table(c)
-
-#    for sublist in c.execute('SELECT * FROM USIODB'):
-#        print sublist
+    
     c.close()
     conn.close()
 
     return render_template('pivot_table.html',table = Markup(table))
 
+
 def html_table(c):
     # Generates table
-    table = '<table class="table table-striped table-hover">'
+    conn = sqlite3.connect('USIODB.db')
+    df = pd.read_sql_query("SELECT * FROM USIODB", conn)
+    
+    return df.to_html(classes="table table-striped").replace('border="1"','border="0"')
 
-    header = 1
 
-    for row in c.execute('SELECT * FROM USIODB'):
 
-        table += '<tr>'
-        if header:
-            table+='<thead>'
-            for data in row:
-                table+='<th>{}</th>'.format(data)
-            table+='</thead></tr>'
-            header = 0
 
-        else:
 
-            for data in row:
-                table+='<td>{}</td>'.format(data)
-            table+='</tr>'
-
-    table+='</table>'
-    return table
 
 @app.route("/pivot_table_builder.html", methods = ['POST','GET'])
 def pivot_table_builder():
     if request.method == 'POST':
                 
         # e.g. ImmutableMultiDict([('colLabel', u'Export Inc/Dec'), ('filterName', u'Service Balance'), ('aggregationOf', u'Minimum of'), ('aggregationCol', u'Service Balance'), ('filterQuery', u'<')])
+        dic = request.form
 
         # Check Validation
-        try:
-            dic = request.form
-            print(dic)
-            
-            colLabel = dic['colLabel']
-            filterName = dic['filterName']
-            filterQuery = dic['filterQuery']
-            aggregationOf = dic['aggregationOf']
-            aggregationCol = dic['aggregationCol']
-            
-        except KeyError:
-            return '400'
-            
+        if len(dic)!=6:
+            raise ValueError
+        else:
+            table_block = build_table(request.form)
+            return table_block.replace('border="1"','border="0"')
+        
+        
+        # Everything seems right, start process data
         return dic['filterValue']
         
             
     return render_template('pivot_table_builder.html')
+
+def build_table(dic):
+    colLabel = dic['colLabel']
+    filterName = dic['filterName']
+    filterQuery = dic['filterQuery']
+    filterValue = dic['filterValue']
+    aggregationOf = dic['aggregationOf']
+    aggregationCol = dic['aggregationCol']
+    
+    conn = sqlite3.connect('USIODB.db')
+    
+    sql = "SELECT Period,{} FROM USIODB WHERE {} {} {}".format(to_valid_query(colLabel),to_valid_query(filterName),filterQuery,filterValue)
+    
+    df = pd.read_sql_query(sql, conn)
+    
+    print(sql)
+    
+    conn.close()
+    return df.to_html()
+    
+
+def to_valid_query(x):
+    return {
+        'Period' : 'Period',
+        'Total Balance' : 'TotalBalance',
+        'Goods Balance' : 'GoodsBalance',
+        'Service Balance' : 'Services_Balance',
+        'Total Exports' : 'Total_Exports',
+        'Export Inc/Dec' : 'Export_Inc_Dec',
+        'Goods Exports' : 'Goods_Exports',
+        'Services Exports' : 'Services_Exports',
+        'Total Imports' : 'Total_Imports',
+        'Import Inc/Dec' : 'Import_Inc_Dec',
+        'Goods Imports' : 'Goods_Imports',
+        'Services Imports' : 'Services_Imports'
+    }[x]
 
 @app.route("/interesting_sights.html")
 def interesting_sights():
